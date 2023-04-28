89d2445249596da1cc52abd9239bb3ff4ec97746
Th3VaultDweller <aztec242@yandex.ru>
2023-04-28 21:34:15 +0300

    Bootstrapified the Edit Profile page

--- members/forms.py
+++ members/forms.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -11,9 +11,12 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 
-                  'first_name', 'last_name',
-                  'password1', 'password2')
+        fields = ('username', 
+                  'email', 
+                  'first_name', 
+                  'last_name',
+                  'password1', 
+                  'password2')
     
     def __init__(self, *args, **kwargs):
         """Функция, позволяющая красиво отобразить поля Username, Password1 и Password2."""
@@ -22,5 +25,22 @@ class SignUpForm(UserCreationForm):
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
         
+class EditProfileForm(UserChangeForm):
+    """Форма для редактирования профилей пользователей блога."""
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-
+    class Meta:
+        model = User
+        fields = ('username', 
+                  'email', 
+                  'first_name', 
+                  'last_name',
+                  'password')
--- members/views.py
+++ members/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
-from .forms import SignUpForm
+from .forms import SignUpForm, EditProfileForm
 
 # Create your views here.
 
@@ -16,7 +16,7 @@ class UserRegisterView(generic.CreateView):
 
 class UserEditView(generic.UpdateView):
     """Форма для редактирования информации существующих пользователей блога."""
-    form_class = UserChangeForm
+    form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
     success_url = reverse_lazy('home')
 

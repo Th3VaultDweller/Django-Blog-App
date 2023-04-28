77813fd5986152dc4c441b5ad3cb4dfa391e557f
Th3VaultDweller <aztec242@yandex.ru>
2023-04-28 17:19:10 +0300

    Updated and Bootstrapified registration fields

---
+++ members/forms.py
@@ -0,0 +1,26 @@
+from typing import Any
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    """Продвинутая форма для регистрации пользователей блога."""
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 
+                  'first_name', 'last_name',
+                  'password1', 'password2')
+    
+    def __init__(self, *args, **kwargs):
+        """Функция, позволяющая красиво отобразить поля Username, Password1 и Password2."""
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        
+
+
--- members/views.py
+++ members/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SignUpForm
 
 # Create your views here.
 
 class UserRegisterView(generic.CreateView):
     """Форма для регистрации новых пользователей блога."""
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 

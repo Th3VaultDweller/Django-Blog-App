8387fe0de4abb5affea379ef474bcddc4310dc2a
Th3VaultDweller <aztec242@yandex.ru>
2023-04-28 19:23:16 +0300

    Added an Edit Profile feature

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
+from typing import Any, Optional
+from django.db import models
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm
 
@@ -11,4 +13,13 @@ class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+class UserEditView(generic.UpdateView):
+    """Форма для редактирования информации существующих пользователей блога."""
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
 

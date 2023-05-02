b13f9626d95fe1669f2c76af23fdfbe5efdb8613
Th3VaultDweller <aztec242@yandex.ru>
2023-05-02 14:34:08 +0300

    Changed the order of fields - Edit Profile Page

--- members/forms.py
+++ members/forms.py
@@ -39,8 +39,8 @@ class EditProfileForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = ('username', 
-                  'email', 
+        fields = ('username',  
                   'first_name', 
                   'last_name',
+                  'email',
                   'password')

3971389702f46cd63d6a137a57436ab5ca9b4509
Th3VaultDweller <aztec242@yandex.ru>
2023-05-05 19:43:09 +0300

    Added a User Profile page in the bio

--- members/urls.py
+++ members/urls.py
@@ -1,12 +1,15 @@
 """Определяет схемы URL для Members."""
 
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     path ('register/', UserRegisterView.as_view(), name ='register'),
     path ('edit_profile/', UserEditView.as_view(), name='edit_profile'),
-    path ('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    path ('password/', PasswordsChangeView.as_view(template_name=
+                                                   'registration/change-password.html'
+                                                   )),
     path ('password_success', views.password_success, name='password_success'),
+    path ('<int:pk>/profile', ShowProfilePageView.as_view(), name='user_profile_page')
 ]
--- members/views.py
+++ members/views.py
@@ -1,13 +1,31 @@
 from typing import Any, Optional
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm
+from blogs.models import UserProfile
 
 # Create your views here.
+
+class ShowProfilePageView(DetailView):
+    """Показывает профиль пользователя блога."""
+    model = UserProfile
+    template_name ='registration/user_profile.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        # users = UserProfile.objects.all() # достаёт все профили из db
+        
+        # достаёт конкретный профиль пользователя из db по pk
+        page_user = get_object_or_404(UserProfile, id=self.kwargs['pk'])
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        context ['page_user'] = page_user
+        return context
+    
+
 
 class UserRegisterView(generic.CreateView):
     """Форма для регистрации новых пользователей блога."""

d90734f8c1c767f0b66effd0233b0f08653d3881
Th3VaultDweller <aztec242@yandex.ru>
2023-05-08 23:36:04 +0300

    App Restoration

--- Blog/settings.py
+++ Blog/settings.py
@@ -120,7 +120,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL ='/media/' # где будут храниться все медиа-файлы
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+   ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, "static"),
+   )
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
